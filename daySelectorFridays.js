9c33c327e0720f4ccb0bd117cbace39f6d518efa
Jorge Verdera <57467445+jverdera19@users.noreply.github.com>
2022-04-07 14:53:54 -0700

    Remote Script for Day Selection - Fridays

---
+++ daySelectorFridays.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+    console.log("Document ready");
+    // Init state, show tuesday, hide friday
+    $(".product-content-tuesday").show();
+    $(".product-content-friday").hide();
+
+    // Set delivery date order
+    if (deliveryDate.includes("Tuesday")) {
+        firstDay = "tuesday";
+    } else {
+        firstDay = "friday";
+    }
+
+    // Check for previously chosen day
+    if (!localStorage.getItem("day")) {
+        localStorage.setItem("day", firstDay);
+    }
+
+    var chosenDay = localStorage.getItem("day");
+
+    let chosenDateFC = "";
+
+    if (firstDay == "tuesday") {
+        if (chosenDay == "tuesday") {
+            chosenDateFC = deliveryDate;
+        } else if (chosenDay == "friday") {
+            chosenDateFC = deliveryDate2;
+        }
+    } else {
+        if (chosenDay == "friday") {
+            chosenDateFC = deliveryDate;
+        } else if (chosenDay == "tuesday") {
+            chosenDateFC = deliveryDate2;
+        }
+    }
+
+    $(".btn[data-day='" + chosenDay + "']").addClass("active");
+    if (chosenDay == "tuesday") {
+        $(".product-tuesday-content").show();
+        $(".product-friday-content").hide();
+    } else if (chosenDay == "friday") {
+        $(".product-tuesday-content").hide();
+        $(".product-friday-content").show();
+    }
+
+    // Choose day
+    $(".btn").click(function () {
+        console.log("Choosing day");
+        $(".btn").removeClass("active");
+        $(this).addClass("active");
+        var day = $(this).attr("data-day");
+
+        localStorage.setItem("day", day);
+
+        if (firstDay == "tuesday") {
+            if (day == "tuesday") {
+                chosenDateFC = deliveryDate;
+                chosenDate = deliveryDate;
+                $(".product-tuesday-content").show();
+                $(".product-friday-content").hide();
+            } else if (day == "friday") {
+                chosenDateFC = deliveryDate2;
+                chosenDate = deliveryDate2;
+                $(".product-tuesday-content").hide();
+                $(".product-friday-content").show();
+            }
+        } else {
+            if (day == "friday") {
+                chosenDateFC = deliveryDate;
+                chosenDate = deliveryDate;
+                $(".product-tuesday-content").hide();
+                $(".product-friday-content").show();
+            } else if (day == "tuesday") {
+                chosenDateFC = deliveryDate2;
+                chosenDate = deliveryDate2;
+                $(".product-tuesday-content").show();
+                $(".product-friday-content").hide();
+            }
+        }
+
+        //Store chosen Date as custom session variable
+        FC.client.request('https://' + FC.settings.storedomain + '/cart?h:chosenDate=' + chosenDateFC).done(function (dataJSON) {
+            console.log("added chosenDateFC", chosenDateFC);
+        });
+    });
+
+    // Add input quantity
+    console.log("Added input quantity")
+
+    // Set delivery date buttons text
+    document.getElementById("deliveryDate").text = deliveryDate;
+    document.getElementById("deliveryDate2").text = deliveryDate2;
+    console.log("completed setting up button text: ", deliveryDate, deliveryDate2);
+});
